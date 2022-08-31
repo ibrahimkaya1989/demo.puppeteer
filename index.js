153f959db6bc0c1b6bf201b6e73aa75c382944f3
İbrahim Kaya <ibrahimkaya1989@windowslive.com>
2022-08-31 13:38:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer'); // Running script : Open terminal and type "node index.js"
+
+(async() => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://tr.wikipedia.org/wiki/Galatasaray_SK');
+    await page.screenshot({ path: 'wiki_gs.png' });
+
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    await delay(5000);
+    console.log("Waited 5s");
+
+    const result = await page.evaluate(() => {
+
+        let headingFromWeb = document.querySelectorAll(".mw-headline");
+        const headingList = [...headingFromWeb];
+        return headingList.map(h => h.innerText);
+    })
+
+    window.scroll(0, 500);
+
+    await delay(5000);
+    console.log("Waited an additional 5s");
+
+    console.log(result);
+
+    await browser.close();
+})();
